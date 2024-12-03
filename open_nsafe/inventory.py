26557bc164e512f5dc7aa8312c4d21db42ae6537
sbradnam <steve.bradnam@ukaea.uk>
2024-12-03 16:34:02 +0000

    json parser and test added

---
+++ open_nsafe/inventory.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+import pypact as pp
+import os
+
+@dataclass
+class Inventory:
+    """Inventory data class
+
+    Attributes
+    ----------
+    times : dict[int, list[float]]
+        Times in inventiry data.
+    masses : dict[int, list[float]]
+        Masses in inventiry data.        
+    """
+    times : dict[str, list[float]]
+    masses : dict[str : list[float]]
+
+    @classmethod
+    def from_json(cls, json_filename : str | os.PathLike, filter : float = 0.0) -> Inventory:
+        times = {}
+        masses = {}
+        with pp.JSONReader(json_filename) as output:
+            for timestamp in output:
+                time = timestamp.irradiation_time/(3600*24)
+                for nuclide in timestamp.nuclides:
+                    isotope = "".join([nuclide.element, str(nuclide.isotope), str(nuclide.state)])
+                    mass = nuclide.grams/1000
+                    if mass > filter:
+                        if (isotope not in times) or (isotope not in masses):
+                            times[isotope] = []
+                            masses[isotope] = []
+                        else:
+                            times[isotope].append(time)
+                            masses[isotope].append(mass)
+        return cls(times = times, masses = masses)
+
+
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,4 +1,14 @@
+import os
+import pytest
+from open_nsafe.inventory import Inventory
+
+cp = os.path.dirname(os.path.abspath(__file__))
+INVENTORY_FILE = os.path.join(cp, '..', 'resources', 'demo_hcpb_fw.json')
+
 class TestInventory:
-    def test_inventory(self):
-        print('Hello World')
-        assert True
+    def test_from_json(self):
+        inventory = Inventory.from_json(INVENTORY_FILE)
+        assert len(inventory.times) == len(inventory.masses)
+        assert len(inventory.times['U239']) == len(inventory.masses['U239'])
+        assert inventory.times['U239'][0] == pytest.approx(60.875)
+        assert inventory.masses['U239'][-1] == pytest.approx(4.59702956595431e-06)
