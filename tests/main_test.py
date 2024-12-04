7acec708c97e4da33c9b6898808cebfbb09fbfb0
sbradnam <steve.bradnam@ukaea.uk>
2024-12-04 13:19:16 +0000

    Moved __main__

--- tests/main_test.py
+++ tests/main_test.py
@@ -3,5 +3,5 @@ from streamlit.testing.v1 import AppTest
 
 class TestMain:
     def test_main(self):
-        app = AppTest(os.path.join("open_nsafe", "__main__.py"), default_timeout=45).run()
+        app = AppTest(os.path.join("app_streamlit.py"), default_timeout=45).run()
         assert True
